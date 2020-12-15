85c010a476b96eb8688d2b1dcd57bff4654b74a9
190800 <190800@e0505-43.JC-21.local>
2020-12-15 15:12:05 +0900

    課題３

--- src/Kadai03.java
+++ src/Kadai03.java
@@ -1,4 +1,3 @@
-
 public class Kadai03 {
 
 	/**
@@ -8,7 +7,33 @@ public class Kadai03 {
 	 * @return sの中に入っていた文字
 	 */
 	char[] CharacterList(String s) {
+		int k;
+		int count =0;
 		char[] c = new char[26];
+		char[] array = s.toCharArray();
+		for(int i=0; i<array.length; i++) {
+			if(array[i] >= 'a' && array[i] <='z') {
+				array[i]= (char)(array[i]-32);
+			}
+			if(array[i] >= 'A' && array[i] <= 'Z') {
+				k = array[i] - 'A';
+				if (c[k] != '*') {
+					c[k] = '*';
+					count++;
+				}
+			}
+		}
+
+		char[] returnc = new char[count];
+		count = 0;
+
+		for(int i=0;i<c.length;i++) {
+			if(c[i]=='*') {
+				returnc[count] = (char)(i +'A');
+				count++;
+
+			}
+		}
 		return c;
 	}
 }
